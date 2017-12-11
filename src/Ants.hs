dd139133a41b90864b09d5511c90c492a9e59307
rexim <reximkut@gmail.com>
2017-12-12 01:18:42 +0700

    Random goals for ants

--- src/Ants.hs
+++ src/Ants.hs
@@ -5,19 +5,25 @@ module Ants ( Ants
 
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
+import Graphics.Gloss.Data.Vector
+import Graphics.Gloss.Geometry.Angle
 import Control.Monad
 import System.Random
+import Debug.Trace
 
 data Role = Soldier | Worker | Gatherer
             deriving (Enum, Show)
 
 data Ant = Ant { antPosition :: Point
                , antHeading :: Float
-               , antRole :: Role }
+               , antRole :: Role
+               , antGoal :: Point }
 
-data Ants = Ants { antsAnts :: [Ant] }
+data Ants = Ants { antsAnts :: [Ant]
+                 , antsStdGen :: StdGen }
 
 worldSize = 800.0
+antSpeed = 100.0
 
 roleColor :: Role -> Color
 roleColor Worker = makeColorI 175 239 239 255
@@ -33,29 +39,54 @@ renderAnt ant = translate x y
                , (20.0, 0.0)
                , (-10.0, -10.0)
                ]
-          heading = antHeading ant
+          heading = radToDeg $ antHeading ant
           (x, y) = antPosition ant
 
-updateAnt :: Float -> Ant -> Ant
-updateAnt deltaTime ant =
-    ant { antHeading = antHeading ant + 180 * deltaTime }
+(!) :: Point -> Point -> Vector
+(!) (x1, y1) (x2, y2) = (x2 - x1, y2 - y1)
+
+(|+|) :: Vector -> Vector -> Vector
+(|+|) (x1, y1) (x2, y2) = (x2 + x1, y2 + y1)
+
+updateAnt :: Float -> StdGen -> Ant -> (Ant, StdGen)
+updateAnt deltaTime g ant
+    | magV distance < 1e-3 =
+        let (x, g1) = randomR (-worldSize, worldSize) g
+            (y, g2) = randomR (-worldSize, worldSize) g1
+        in ( ant { antGoal = (x, y)
+                 , antHeading = argV ((antPosition ant) ! (traceShowId (x, y))) }
+           , g2 )
+    | otherwise =
+        let heading = argV distance
+            position = antPosition ant
+        in ( ant { antPosition = position |+| (mulSV (antSpeed * deltaTime) $ unitVectorAtAngle heading)
+                 , antHeading = heading }
+           , g )
+    where distance = (antPosition ant) ! (antGoal ant)
 
 randomAnt :: IO Ant
 randomAnt = do x <- randomRIO (-worldSize, worldSize)
                y <- randomRIO (-worldSize, worldSize)
-               heading <- randomRIO (0.0, 360.0)
+               heading <- randomRIO (0.0, 2 * pi)
                role <- toEnum <$> randomRIO (0, 2)
                return $ Ant { antPosition = (x, y)
                             , antHeading = heading
-                            , antRole = role }
+                            , antRole = role
+                            , antGoal = (x, y) }
 
 initialAnts :: Int -> IO Ants
 initialAnts n = do ants <- replicateM n randomAnt
-                   return $ Ants { antsAnts = ants } -- ants
+                   return $ Ants { antsAnts = ants
+                                 , antsStdGen = mkStdGen 42 }
 
 renderAnts :: Ants -> Picture
 renderAnts = pictures . map renderAnt . antsAnts
 
 updateAnts :: ViewPort -> Float -> Ants -> Ants
-updateAnts _ deltaTime state = state { antsAnts = map (updateAnt deltaTime)
-                                                  $ antsAnts state }
+updateAnts _ deltaTime state = let (ants', gp) = foldl (\(ants, g) ant ->
+                                                            let (ant', g') = updateAnt deltaTime g ant
+                                                            in ((ant':ants), g'))
+                                                       ([], antsStdGen state)
+                                                       (antsAnts state)
+                               in state { antsAnts = ants'
+                                        , antsStdGen = gp }
